caa37f618510a67e1d8945a3ce106eb5303142df
Bruce Becker <brucellino@gmail.com>
2017-05-05 18:17:20 +0200

    Prepare for FR 3 release
      1. Give the license to CSIR
      1. Include license text in all files
      1. Update modulefiles with new var from the new containers

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash -e
+# Copyright 2016 C.S.I.R. Meraka Institute
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # deploy script for java openjdk
 
 # Build only downloads and unpacks the code to the install dir.
@@ -11,7 +25,7 @@ echo "now deploying to ${SOFT_DIR}"
 cd ${WORKSPACE}/${NAME}-${VERSION}
 echo "Java $VERSION will now go into ${SOFT_DIR}/"
 mkdir -p ${SOFT_DIR}
-mkdir -p ${LIBRARIES_MODULES}/${NAME}
+mkdir -p ${LIBRARIES}/${NAME}
 tar xfz ${SRC_DIR}/${SOURCE_FILE} -C ${SOFT_DIR} --skip-old-files --strip-components=1
 
 # TODO - see if Java works.
@@ -31,7 +45,7 @@ setenv JAVA_HOME                           $::env(CVMFS_DIR)/$::env(SITE)/$::env
 prepend-path LD_LIBRARY_PATH    $::env(JAVA_DIR)/lib
 prepend-path PATH                            $::env(JAVA_DIR)/bin
 MODULE_FILE
-) > ${LIBRARIES_MODULES}/${NAME}/${VERSION}
+) > ${LIBRARIES}/${NAME}/${VERSION}
 
 
 echo "Checking java module"
